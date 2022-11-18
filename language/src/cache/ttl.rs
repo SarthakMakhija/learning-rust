9a9a4402c277f7312635803676cd2fd334c720b7
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-11-18 14:26:36 +0530

    Sarthak | Adds the concept of expiry

--- language/src/cache/ttl.rs
+++ language/src/cache/ttl.rs
@@ -1,3 +1,5 @@
+use std::mem::transmute;
+use std::thread;
 use std::time::Duration;
 
 use tokio::time::Instant;
@@ -15,18 +17,53 @@ impl ValueRef {
     pub fn value(&self) -> &String {
         return &self.value;
     }
+
+    pub fn has_expired(&self) -> bool {
+        return match self.expiry_after.instant {
+            None => { false }
+            Some(expire_after) => {
+                let now = Instant::now();
+                expire_after.saturating_duration_since(now) == Duration::from_nanos(0)
+            }
+        };
+    }
 }
 
 pub struct Expiry {
-    duration: Option<Instant>,
+    instant: Option<Instant>,
 }
 
 impl Expiry {
     pub fn never() -> Expiry {
-        return Expiry { duration: None };
+        return Expiry { instant: None };
     }
 
     pub fn after_seconds(time: u64) -> Expiry {
-        return Expiry { duration: Some(Instant::now() + Duration::from_secs(time)) };
+        return Expiry { instant: Some(Instant::now() + Duration::from_secs(time)) };
     }
+}
+
+#[test]
+fn test_a_non_expiry_with_a_never_expiring_value() {
+    let value_ref = ValueRef::new(String::from("some value"), Expiry::never());
+    let has_expired = value_ref.has_expired();
+
+    assert_eq!(false, has_expired);
+}
+
+#[test]
+fn test_a_non_expiry_with_an_expiring_value() {
+    let value_ref = ValueRef::new(String::from("some value"), Expiry::after_seconds(10));
+    let has_expired = value_ref.has_expired();
+
+    assert_eq!(false, has_expired);
+}
+
+#[test]
+fn test_an_expiry() {
+    let value_ref = ValueRef::new(String::from("some value"), Expiry::after_seconds(1));
+    thread::sleep(Duration::from_secs(2));
+
+    let has_expired = value_ref.has_expired();
+    assert_eq!(true, has_expired);
 }
